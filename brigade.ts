e122dc20a89304bdc6731a6e5530a471e2e0ac7e
Jakub Borys <jakub.borys@gmail.com>
2019-03-22 11:33:12 +0000

    Ignore dist

--- brigade.ts
+++ brigade.ts
@@ -1,5 +1,7 @@
 import { events } from "@azure/brigadier";
 
-events.on("exec", e => {
-  console.log(e);
+events.on("exec", (event, project) => {
+  console.log("Hello Brigade");
+  console.log(event);
+  console.log(project);
 });
