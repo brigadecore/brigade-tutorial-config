30056a0c90ba99480cc95e0108cbfbc48eb7e554
Jakub Borys <jakub.borys@gmail.com>
2019-03-22 15:14:47 +0000

    TS Build on worker start

--- brigade.ts
+++ brigade.ts
@@ -1,7 +1,7 @@
 import { events } from "@azure/brigadier";
 
 events.on("exec", (event, project) => {
-  console.log("Hello Brigade");
+  console.log("Hello Brigade!");
   console.log(event);
   console.log(project);
 });
